28ab5674524330d849e777aea4296cba732ca66c
Matheus Rosendo <matheus.rosendo@lactec.org.br>
2022-06-22 10:19:28 -0300

    Updates

---
+++ precache-manifest.d0ee2b949c54e70d947f6a8beddacee6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d2818179da44552f40bcaaaad8fa23",
+    "url": "/TokenizationLab/index.html"
+  },
+  {
+    "revision": "4f852bb3a4e1c6e4b5dc",
+    "url": "/TokenizationLab/static/css/main.8e9eaac7.chunk.css"
+  },
+  {
+    "revision": "bf20e4a816fee998a6ad",
+    "url": "/TokenizationLab/static/js/2.7f2948f7.chunk.js"
+  },
+  {
+    "revision": "4f852bb3a4e1c6e4b5dc",
+    "url": "/TokenizationLab/static/js/main.05bc61ce.chunk.js"
+  },
+  {
+    "revision": "a4fcbb17b6c2931cd5ee",
+    "url": "/TokenizationLab/static/js/runtime-main.352ba497.js"
+  }
+]);
